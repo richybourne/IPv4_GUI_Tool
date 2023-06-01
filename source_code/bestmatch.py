6ffcd30fc13f6cff31603c2a09e716739c689544
ricardo <rickyaevarg@gmail.com>
2023-05-31 20:42:11 -0700

    Updated bug in best match algorithm

--- source_code/bestmatch.py
+++ source_code/bestmatch.py
@@ -2,36 +2,42 @@ import ipaddress
 
 def perform_ipv4_best_match_lookup(destination_ip, networks):
     # Step 1: Convert the destination and networks to binary
-    destination_ip = destination_ip.split('/')[0]
-    destination_ip = '{:#b}'.format(ipaddress.IPv4Address(destination_ip))
-    destination_ip = destination_ip[2:]
+    destination_ip_binary = '{:#b}'.format(ipaddress.IPv4Address(destination_ip))
+    destination_ip_binary = destination_ip_binary[2:]
 
+    # Make a new array of tuples, the left having the binary network address and the right having the subnet mask
     binary_networks = []
     for network in networks:
-        network = network.split('/')[0]
-        network = '{:#b}'.format(ipaddress.IPv4Address(network))
-        network = network[2:]
-        binary_networks.append(network)
+        bin_network = network.split('/')[0]
+        bin_network = '{:#b}'.format(ipaddress.IPv4Address(bin_network))
+        bin_network = bin_network[2:]
+        binary_networks.append(bin_network)
+    bin_networks_tuple = []
+    for i in range(len(binary_networks)):
+        bin_networks_tuple.append((binary_networks[i], networks[i].split('/')[1]))
+
+    # Cut each binary network address to the length of the subnet mask
+    final_cut_networks = [(network[0][:int(network[1])], network[1]) for network in bin_networks_tuple]
 
     # Step 2: Find the longest common prefix (number of matching bits)
-    current_longest_prefix = 0
-    current_longest_prefix_network = None
-    for network in binary_networks:
-        number_of_matching_bits = 0
-        for i in range(len(network)):
-            if network[i] == destination_ip[i]:
-                number_of_matching_bits += 1
-            else: break
-        if number_of_matching_bits > current_longest_prefix:
-            current_longest_prefix = number_of_matching_bits
-            current_longest_prefix_network = network
-    
-    # Step 3: Copy the matching bits and add all 0 bits to determine the network address
-    network_address = current_longest_prefix_network[:current_longest_prefix]
-    network_address += '0' * (32 - current_longest_prefix)
-    network_address = ipaddress.IPv4Address(int(network_address, 2))
+    longest_match_subnet = 0
+    longest_match_index = 0
+    for i in range(len(final_cut_networks)):
+        current_network = final_cut_networks[i][0]
+        current_subnet_mask = final_cut_networks[i][1]
+        temp_destination_ip = destination_ip_binary[:int(current_subnet_mask)]
+        if temp_destination_ip == current_network:
+            if int(current_subnet_mask) > longest_match_subnet:
+                longest_match_subnet = int(current_subnet_mask)
+                longest_match_index = i
 
-    # Turn the address and subnet mask into a CIDR notation
-    return str(network_address) + '/' + str(current_longest_prefix)
+    # Step 3: Return the network address and subnet mask of the longest match
+    if longest_match_subnet == 0:
+        return 'No match found'
+
+    return networks[longest_match_index]
+
+    
+
 
     
